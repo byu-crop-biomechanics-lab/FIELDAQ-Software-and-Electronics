17a283548b35853d4e51cadd3007268c3b7d5286
jaredhmt <jaredhmt@gmail.com>
2019-06-10 15:46:01 -0600

    fixed an issue with plotting scaling not working with a 0 in the dataset.

--- Granusoft/src/view/screens/main/testing/TestInProgressScreen.py
+++ Granusoft/src/view/screens/main/testing/TestInProgressScreen.py
@@ -102,16 +102,20 @@ class TestInProgressScreen(BaseScreen):
             self.plot2 = MeshLinePlot(color=[1, 1, 1, 1])
             last_index = len(self.datasets) - 1
 
+            try:
+                self.y_max1ph = math.ceil(max(self.datasets[i].pot_angle for i in range(0,len(self.datasets))) / 250) * 250
+            except:
+                self.y_max1ph = 500
             self.x_max = math.ceil(self.datasets[last_index].timestamp / 5) * 5
-            self.y_max1 = math.ceil(max(self.datasets[i].pot_angle for i in range(0,len(self.datasets))) / 225) * 250
-            self.y_max2 = math.ceil(max(self.datasets[i].x_load for i in range(0,len(self.datasets))) / 225) * 250
+            self.y_max1 = max(500, self.y_max1ph)
+            #self.y_max2 = math.ceil(max(self.datasets[i].x_load for i in range(0,len(self.datasets))) / 225) * 250
             #if(self.find_max_x_load() == 0):
              #   self.y_max = 10000
             #else:
             #    self.y_max = math.ceil(self.find_max_x_load() / 10000) * 10000
             self.x_major = int(self.x_max/5)
             self.y_major1 = int(self.y_max1/5)
-            self.y_major2 = int(self.y_max2/5)
+            #self.y_major2 = int(self.y_max2/5)
             #self.y_major = int(self.y_max/5)
 
 
