93289619d885c9368cc2c4bd7e53fe53ffc802ea
jaredhmt <jaredhmt@gmail.com>
2019-06-12 18:13:27 -0600

    Added update software os functionality in settings screen

--- Granusoft/src/view/screens/main/SettingsScreen.py
+++ Granusoft/src/view/screens/main/SettingsScreen.py
@@ -48,3 +48,8 @@ class SettingsScreen(BaseScreen):
     def save(self, path, filename):
         config.save_as(os.path.join(path, filename))
         self.dismiss_popup()
+
+    def updateOS(self):
+        app.stop()
+        os.system("git pull")
+        os.system("python3 main.py")
