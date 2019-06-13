f3312f23eb470446869c8e2d083385ca54409f43
jaredhmt <jaredhmt@gmail.com>
2019-06-13 12:56:03 -0600

    Undid moving to new screen due to complications with implementation

--- Granusoft/src/view/screens/settings/SoftwareUpdateScreen.py
+++
@@ -1,8 +0,0 @@
-from kivy.lang import Builder
-
-from view.BaseScreen import BaseScreen
-
-Builder.load_file('view/screens/settings/SoftwareUpdateScreen.kv')
-
-class SoftwareUpdateScreen(BaseScreen):
-    pass
