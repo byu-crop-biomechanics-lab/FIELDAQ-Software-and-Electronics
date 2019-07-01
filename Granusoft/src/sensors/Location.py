37581237ce6649d85e913f2e0cc603e1e6c85f07
JaredHMT <jaredhmt@gmail.com>
2019-07-01 15:49:29 -0600

    Fixed issue of GPS lagging, this came at the expense of significant performance issues. The device is now extremely slow. All gps tests though are up to date. Either all screens need to update the gps location in a loop, or some other change needs to occur to the gps update method.

--- Granusoft/src/sensors/Location.py
+++ Granusoft/src/sensors/Location.py
@@ -1,4 +1,5 @@
 from .connections import *
+from kivy.clock import Clock
 
 class Location:
 
@@ -9,11 +10,21 @@ class Location:
         if gps.has_fix:
             self.lat = gps.latitude
             self.long = gps.longitude
+        self.Event = Clock.schedule_interval(self.data_refresh, 5)
 
     def get_data(self):
         gps.update()
         if gps.has_fix:
-            gps.update()
             self.lat = gps.latitude
             self.long = gps.longitude
         return self.lat, self.long
+
+    def data_refresh(self, obj):
+        gps.update()
+        if gps.has_fix:
+            self.lat = gps.latitude
+            self.long = gps.longitude
+        return self.lat, self.long
+
+    def __del__(self):
+        self.Event.cancel()
