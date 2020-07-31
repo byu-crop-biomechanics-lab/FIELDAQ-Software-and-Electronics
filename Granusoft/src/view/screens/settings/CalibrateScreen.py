32bb907b1b3c1e421e9dbeac76cdbc80b8259f86
jaredhmt <jaredhmt@gmail.com>
2020-07-31 14:46:12 -0600

    Added recalculation of intercept and slope as well as commented out all print messages.

--- Granusoft/src/view/screens/settings/CalibrateScreen.py
+++ Granusoft/src/view/screens/settings/CalibrateScreen.py
@@ -64,22 +64,32 @@ class CalibrateScreen(BaseScreen):
             self.intercept = 0.0
 
     def remove_point(self, obj):
-        print(self.sensor_name) #"We Should Remove This(/These) Point(s) for Sensor" + self.sensor_name)
+        #print(self.sensor_name) #"We Should Remove This(/These) Point(s) for Sensor" + self.sensor_name)
         #print("Which Tests?")
-        for items in self.points_list: print(items)
-        print("remove these tests:")
+        #for items in self.points_list: print(items)
+        #print("remove these tests:")
         selection = self.points_List.get_selected()
         for items in selection:
-            print(items)
+            #print(items)
             ADC = str(items[0])
             REAL = str(items[1])
             for elem in self.points_list:
                 if ADC == str(elem[0]) and REAL == str(elem[1]):
                     try:
                         self.points_list.remove(elem)
-                        print("Removed Point (" + ADC + ", " + REAL + ")")
+                        #print("Removed Point (" + ADC + ", " + REAL + ")")
                     except:
                         pass
+        # Calculate line of best fit using Least Square Method
+        adc_points = [x[0] for x in self.points_list]
+        real_points = [x[1] for x in self.points_list]
+        if len(self.points_list) > 1:
+            poly = numpy.polyfit(adc_points, real_points, 1) # Linear regression
+            self.slope = numpy.float(poly[0])
+            self.intercept =numpy.float(poly[1])
+        else:
+            self.slope = 1.0
+            self.intercept = 0.0
 
     def save(self):
         self.config_data[self.sensor_name] = {
