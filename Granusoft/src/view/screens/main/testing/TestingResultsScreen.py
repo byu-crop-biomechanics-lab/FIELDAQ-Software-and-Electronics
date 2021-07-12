375821f8d0da719b741a6eb9516d30107a06d633
Brandon Sutherland <brandonsutherland2@gmail.com>
2021-07-12 14:11:24 -0600

    Updated version number. 2.2.0 adds post-test viewing screen.

--- Granusoft/src/view/screens/main/testing/TestingResultsScreen.py
+++ Granusoft/src/view/screens/main/testing/TestingResultsScreen.py
@@ -135,7 +135,7 @@ class TestingResultsScreen(BaseScreen):
         with open(filename, 'w+', newline='') as csvFile:
             writer = csv.writer(csvFile)
             writer.writerow(['----------META DATA----------'])
-            writer.writerow(['SOFTWARE VERSION', '2.1.2'])
+            writer.writerow(['SOFTWARE VERSION', '2.2.0'])
             writer.writerow(['DEVICE OPERATOR', str(config.get('operator', 0))])
             writer.writerow(['----------TEST ATTRIBUTES----------'])
             writer.writerow(['FIELD', 'VALUE', 'UNIT'])
