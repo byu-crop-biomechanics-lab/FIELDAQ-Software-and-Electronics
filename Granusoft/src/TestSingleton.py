6d082a483fb9f7c525f57e46b8c08d940e23d3ce
Brandon Sutherland <brandonsutherland2@gmail.com>
2021-08-11 05:29:27 -0600

    Added barcode scanning functionality for individual tests

--- Granusoft/src/TestSingleton.py
+++ Granusoft/src/TestSingleton.py
@@ -6,7 +6,7 @@ class TestSingleton:
             self.height = ""
             self.plot = ""
             self.pre_notes = ""
-            self.post_notes = ""
+            self.post_notes = []
             self.operator = ""
             self.timestamp = ""
             self.datasets = []
@@ -15,10 +15,10 @@ class TestSingleton:
     def __init__(self):
         if not TestSingleton.instance:
             TestSingleton.instance = TestSingleton.__TestSingleton()
-            
+
     def clear_all(self):
         self.instance.clear_all()
-        
+
     def print_test(self):
         print("Timestamp:", self.instance.timestamp)
         print("Height:", self.instance.height)
@@ -29,52 +29,52 @@ class TestSingleton:
         print("Post Notes:", self.instance.post_notes)
         print("First Dataset Temp:", self.instance.datasets[0].temperature)
         print("Second Dataset Temp:", self.instance.datasets[1].temperature)
-        
+
     def get_height(self):
         return self.instance.height
     def set_height(self, height):
         self.instance.height = height
-        
+
     def get_plot(self):
         return self.instance.plot
     def set_plot(self, plot):
         self.instance.plot = plot
-        
+
     def get_pre_notes(self):
         return self.instance.pre_notes
     def set_pre_notes(self, pre_notes):
         self.instance.pre_notes = pre_notes
-        
+
     def get_post_notes(self):
         return self.instance.post_notes
     def set_post_notes(self, post_notes):
         self.instance.post_notes = post_notes
-        
+
     def get_operator(self):
         return self.instance.operator
     def set_operator(self, operator):
-        self.instance.operator = operator        
-        
+        self.instance.operator = operator
+
     def get_timestamp(self):
         return self.instance.timestamp
     def set_timestamp(self, timestamp):
-        self.instance.timestamp = timestamp        
+        self.instance.timestamp = timestamp
 
     def get_datasets(self):
         return self.instance.datasets
     def set_datasets(self, datasets):
-        self.instance.datasets = datasets  
+        self.instance.datasets = datasets
 
     def get_break_height(self):
         return self.instance.break_height
     def set_break_height(self, break_height):
         self.instance.break_height = break_height
-        
+
 #class Dataset:
 #    def __init__(self, name, test):
 #        self.name = name
 #        self.number = test
-        
+
 #a = TestSingleton()
 #a.set_height("tall")
 #d1 = Dataset("ben", "1")
@@ -98,6 +98,3 @@ class TestSingleton:
 #print(b.get_datasets()[0].name)
 #print("a is",a.get_height())
 #print(a.get_datasets()[0].name)
-
-
-
--- Granusoft/src/view/screens/main/testing/TestingResultsScreen.py
+++ Granusoft/src/view/screens/main/testing/TestingResultsScreen.py
@@ -4,12 +4,7 @@ Shows all data: Temperature, Humidity, Location, Time, and all Sensor data
 
 from kivy.lang import Builder
 from kivy.properties import NumericProperty
-from kivy.properties import StringProperty
-from kivy.properties import ListProperty
-from kivy.clock import Clock
 from Sensor import Sensor
-import datetime
-import time
 import math
 from TestSingleton import TestSingleton
 
@@ -17,18 +12,14 @@ from view.BaseScreen import BaseScreen
 from view.StaticList import StaticList
 from view.elements import *
 import configurator as config
-import csv
 try:
     from sensors.connections import *
 except:
     pass
 
-from kivy.garden.graph import Graph, MeshLinePlot
+from kivy.garden.graph import MeshLinePlot
 
 Builder.load_file('view/screens/main/testing/TestingResultsScreen.kv')
-
-ONE_SEC = 1
-
 
 class TestingResultsScreen(BaseScreen):
     x_max = NumericProperty(1)
@@ -43,6 +34,7 @@ class TestingResultsScreen(BaseScreen):
             if(dataset.x_load > max):
                 max = dataset.x_load
         return max
+
     def on_pre_enter(self):
         sensor = Sensor()
         sensor.clear_gps_memory()
@@ -75,84 +67,11 @@ class TestingResultsScreen(BaseScreen):
 
         self.graph.add_plot(self.plot)
 
-    def save_test(self):
+    def save_post_notes(self):
+        """Saves selected post test notes."""
+
         ts = TestSingleton()
-        self.datasets = ts.get_datasets()
-        ts.set_break_height(str(config.get('break_height', 0)))
-
-        #Prepare the notes
-        notes = config.get('notes', {
-            "pretest": [],
-            "bank": []
-        })
-        pre_notes = notes["pretest"]
-        post_notes = self.ids["posttest"].remove_selected()
-        dt = datetime.datetime.now()
-        filename = 'Tests/' + dt.strftime('%Y_%m_%d_%H_%M_%S') + '.csv'
-
-        try:
-            gps.update()
-        except:
-            pass
-        sensor = Sensor()
-        sensor.get_header_data()
-        sensor_data = sensor.get_sensor_data()
-        temperature = str(sensor_data["Temperature"])
-        humidity = str(sensor_data["Humidity"])
-        location = [str("%.7f" % sensor_data["Location"][0]), str("%.7f" % sensor_data["Location"][1])]
-
-        self.config_data = config.get('sensors', {})
-        self.NAMES = ['X Load', 'Y Load', 'IMU Angle', 'Pot Angle', 'Temperature', 'Humidity']
-        self.SENSOR = ['LOAD_X', 'LOAD_Y', 'IMU', 'POT', 'TEMP', 'HUM']
-        self.UNITS = ['N', 'Newtons', 'Deg', 'Deg', 'C', '%']
-        self.IDS = ['loadx1', 'loady1', 'imu1', 'pot1', 'temp1', 'hum1']
-
-        with open(filename, 'w+', newline='') as csvFile:
-            writer = csv.writer(csvFile)
-            writer.writerow(['----------META DATA----------'])
-            writer.writerow(['SOFTWARE VERSION', '2.2.1'])
-            writer.writerow(['DEVICE OPERATOR', str(config.get('operator', 0))])
-            writer.writerow(['----------TEST ATTRIBUTES----------'])
-            writer.writerow(['FIELD', 'VALUE', 'UNIT'])
-            writer.writerow(['YEAR', dt.strftime("%Y")])
-            writer.writerow(['MONTH', dt.strftime("%m")])
-            writer.writerow(['DAY', dt.strftime("%d")])
-            writer.writerow(['TIME', dt.strftime("%H:%M:%S"), 'Local Time Zone'])
-            writer.writerow(['PLOT', str(config.get('plot_num', 0)), '#'])
-            writer.writerow(['HEIGHT', str(config.get('height', 0)), 'cm'])
-            writer.writerow(['BARCODE', 'FIXME: Add barcode data'])
-            writer.writerow(['TEMPERATURE', temperature, 'C'])
-            writer.writerow(['HUMIDITY', humidity, '%'])
-            writer.writerow(['LATITUDE', location[0], 'angular degrees'])
-            writer.writerow(['LONGITUDE', location[1], 'angular degrees'])
-            writer.writerow(['----------OPTIONAL DATA----------'])
-            for i in range(5):
-                try:
-                    writer.writerow(['PRE_TEST_NOTE_' + str(i+1), pre_notes[i]])
-                except:
-                    writer.writerow(['PRE_TEST_NOTE_' + str(i+1), ''])
-            for i in range(5):
-                try:
-                    writer.writerow(['POST_TEST_NOTE_' + str(i+1), post_notes[i]])
-                except:
-                    writer.writerow(['POST_TEST_NOTE_' + str(i+1), ''])
-            writer.writerow(['BREAK_HEIGHT', str(config.get('break_height', 0)), 'cm'])
-            writer.writerow(['LCA_WEIGTH', '0', 'g'])
-            writer.writerow(['----------SENSOR CALIBRATION DATA (stored_value*A + B = raw_data)------'])
-            writer.writerow(['SENSOR', 'A', 'B', 'UNIT', 'ID'])
-            for j in range(len(self.NAMES)):
-                try:
-                    writer.writerow([self.SENSOR[j], self.config_data[self.NAMES[j]]['slope'], self.config_data[self.NAMES[j]]['intercept'], self.UNITS[j], self.IDS[j]])
-                except:
-                    writer.writerow([self.SENSOR[j], '1', '0', self.UNITS[j], self.IDS[j]])
-            writer.writerow(['----------TEST DATA-----------'])
-            writer.writerow(['TIME (s)', 'ANGLE_POT', 'ANGLE_IMU', 'LOAD_X', 'LOAD_Y'])
-            datasets = ts.get_datasets()
-            for ds in datasets:
-                writer.writerow([ds.timestamp, ds.pot_angle, ds.imu_angle, ds.x_load, ds.y_load])
-
-
-        csvFile.close()
+        ts.set_post_notes(self.ids["posttest"].remove_selected())
 
     def on_leave(self):
         self.graph.remove_plot(self.plot)
