6f2f455562e2682e4cf0f071e178f63c21316cd6
Caleb <calebsp12@gmail.com>
2023-10-24 08:24:50 -0600

    Base screen for mode select built

---
+++ Granusoft/src/view/screens/main/ModeSelect.py
@@ -0,0 +1,29 @@
+"""
+The main screen contains four buttons for navigation:
+Settings, Testing, Live Feed, and Exit
+
+It also shows environment data: Temperature, Humidity, Location, and Time.
+"""
+
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+from kivy.clock import Clock
+from view.BaseScreen import BaseScreen
+from Sensor import Sensor
+import datetime
+# import pytz
+# from timezonefinder import TimezoneFinder
+
+Builder.load_file('view/screens/main/ModeSelect.kv')
+
+
+class ModeSelect(BaseScreen):
+    
+    def on_pre_enter(self):
+       pass
+    def on_enter(self):
+        pass
+
+    def on_leave(self):
+        self.event.cancel()
+        self.event2.cancel()
