c232f025f846b084d4e302db7366f704c2abd72e
Caleb Price <106776391+pr1ce1227@users.noreply.github.com>
2023-10-30 09:50:17 -0600

    Mode select
    * Base screen for mode select built
    
    * Mode select functioning, need to rebuild file structure
    
    * started file structure
    
    * continued File structure
    
    * New src working on ARM
    
    * File structure complete for ARM and Camera
    
    * Forgot to save a file

---
+++ Granusoft/src_new/Arm/Screens/Settings/PlotScreen.py
@@ -0,0 +1,42 @@
+"""
+An input text box that, when selected, allows the user to type in the current Plot
+setting via a touch screen number pad that will pop up. The value in the input text box
+when you first visit this view is whatever value for the Plot setting is currently stored
+in our settings file.
+"""
+
+from kivy.lang import Builder
+
+import configurator as config
+from util.BaseScreen import BaseScreen
+from util.input.IntInput import IntInput
+from util.getKVPath import getKVPath
+import os
+
+Builder.load_file(getKVPath(os.getcwd(), __file__))
+
+class PlotScreen(BaseScreen):
+    def on_pre_enter(self):
+        """Before the Screen loads, read the configuration file to get the current
+        plot number."""
+        input = self.ids['plot_num']
+        input.text = str(config.get('plot_num', 1))
+        input.validate()
+
+    def on_enter(self):
+        """Once the Screen loads, focus the TextInput"""
+        input = self.ids['plot_num']
+        input.focus = True
+        #input.select_all()
+
+    def save(self):
+        """Save button was pressed: save the new height in the configuration file.
+        Returns True if save was successful.  False otherwise."""
+        input = self.ids['plot_num']
+        valid = input.validate()
+        if valid:
+            config.set('plot_num', int(input.text))
+            return True
+        else:
+            input.focus = True
+            return False
