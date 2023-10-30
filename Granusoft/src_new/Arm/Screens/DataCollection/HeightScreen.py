6ae17b18d282b8cf5e137bfbc5fe1209b3c85252
Caleb Price <106776391+pr1ce1227@users.noreply.github.com>
2023-10-30 09:50:17 -0600

    Mode select
    * Base screen for mode select built
    
    * Mode select functioning, need to rebuild file structure
    
    * started file structure
    
    * continued File structure
    
    * New src working on ARM
    
    * File structure complete for ARM and Camera
    
    * Forgot to save a file

---
+++ Granusoft/src_new/Arm/Screens/DataCollection/HeightScreen.py
@@ -0,0 +1,42 @@
+"""
+An input text box that, when selected, allows the user to type in the current Height
+setting via a touch screen number pad that will pop up. The value in the input text box
+when you first visit this view is whatever value for the Height setting is currently
+stored in our settings file.
+"""
+
+from kivy.lang import Builder
+
+import configurator as config
+from util.BaseScreen import BaseScreen
+from util.input.FloatInput import FloatInput
+from util.getKVPath import getKVPath
+import os
+
+Builder.load_file(getKVPath(os.getcwd(), __file__))
+
+class HeightScreen(BaseScreen):
+    def on_pre_enter(self):
+        """Before the Screen loads, read the configuration file to get the current
+        height."""
+        input = self.ids['height']
+        input.text = str(config.get('height', 0))
+        input.validate()
+
+    def on_enter(self):
+        """Once the Screen loads, focus the TextInput"""
+        input = self.ids['height']
+        input.focus = True
+        #input.select_all()
+
+    def save(self):
+        """Save button was pressed: save the new height in the configuration file.
+        Returns True if save was successful.  False otherwise."""
+        input = self.ids['height']
+        valid = input.validate()
+        if valid:
+            config.set('height', float(input.text))
+            return True
+        else:
+            input.focus = True
+            return False
---
+++ Granusoft/src_new/Camera/Screens/ImageDeleteConfirmation.py
@@ -0,0 +1,29 @@
+"""
+This screen makes sure that the user will not accidentally delete images stored on the device. It is quite simple
+with only two buttons, delete and cancel. The position and type of button is intentionally different than the previous
+set of buttons so it is very obvious there was a change in screen. 
+"""
+
+import os
+
+from kivy.lang import Builder
+
+from util.BaseScreen import BaseScreen
+from os import listdir
+from os.path import isfile, join
+from util.getKVPath import getKVPath
+
+Builder.load_file(getKVPath(os.getcwd(), __file__))
+
+class ImageDeleteConfirmation(BaseScreen):
+    def on_pre_enter(self):
+        self.image_filenames = [f for f in listdir("Images") if (isfile(join("Images", f)) and f != ".gitignore")]
+
+    def remove_all(self):
+        for name in self.image_filenames:
+            if name != '.gitignore':
+                os.remove('Images/' + name)
+        super(ImageDeleteConfirmation, self).back()
+
+    def cancel(self):
+        super(ImageDeleteConfirmation, self).back()
