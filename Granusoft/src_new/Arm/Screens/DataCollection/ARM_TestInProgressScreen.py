396a3eabae4ad4612d52abcb90d1cb5d1fcaea8f
Nate Ludlow <nateludlow@gmail.com>
2023-11-07 08:46:40 -0700

    added files to tracking

---
+++ Granusoft/src_new/Arm/Screens/DataCollection/ARM_TestInProgressScreen.py
@@ -0,0 +1,155 @@
+"""
+Test in Progress
+"""
+
+import datetime
+
+from kivy.lang import Builder
+from kivy.properties import NumericProperty
+from kivy.properties import StringProperty
+from kivy.properties import ListProperty
+from kivy.clock import Clock
+from Arm.Data.Dataset import Dataset
+from Arm.Sensors import Sensor
+from Arm.Data.TestSingleton import TestSingleton
+
+from util.BaseScreen import BaseScreen
+import Arm.Settings.configurator as config
+from util.elements import *
+import datetime
+import time
+import math
+
+from kivy.garden.graph import MeshLinePlot
+from util.getKVPath import getKVPath
+import os
+
+Builder.load_file(getKVPath(os.getcwd(), __file__))
+
+INTERVAL = .003
+SECOND_CAP = 1/INTERVAL
+
+class ARM_TestInProgressScreen(BaseScreen):
+    test_time = NumericProperty(0)
+    x_max = NumericProperty()
+    x_min = NumericProperty()
+    y_max1 = NumericProperty()
+    y_max2 = NumericProperty()
+    y_min1 = NumericProperty()
+    y_min2 = NumericProperty()
+    x_major = NumericProperty()
+    y_major1 = NumericProperty()
+    y_major2 = NumericProperty()
+    temperature = 0
+    humidity = 0
+    location = 0
+    x_load = 0
+    y_load = 0
+    pot_angle = 0
+    imu_angle = 0
+    data_rate = 0
+    second_counter = 0
+    double_counter = 0
+    start_time = 0
+    start_timestamp = datetime.datetime.now().strftime("%I:%M:%S %p")
+
+    def on_pre_enter(self):
+        self.test_time = 0
+        self.temperature = 0
+        self.humidity = 0
+        self.location = 0
+        self.x_load = 0
+        self.y_load = 0
+        self.pot_angle = 0
+        self.imu_angle = 0
+        self.data_rate = 0
+        self.strain1 = 0
+        self.strain2 = 0
+        self.second_counter = 0
+        self.double_counter = 0
+        self.start_time = datetime.datetime.now()
+        self.datasets = []
+        self.x_min = 0
+        self.y_min1 = 0
+        self.y_min2 = 0
+        self.y_max1 = 100
+        self.y_max2 = 100
+        self.x_major = int((self.x_max-self.x_min)/5)
+        self.y_major1 = int((self.y_max1-self.y_min1)/5)
+        self.y_major2 = int((self.y_max2-self.y_min2)/5)
+        self.datasets = []
+        self.test_sensor = Sensor()
+        self.plot1 = MeshLinePlot(color=[1, 1, 1, 1])
+        self.plot2 = MeshLinePlot(color=[1, 1, 1, 1])
+
+        self.event = Clock.schedule_interval(self.update_dataset, INTERVAL)
+        #ClockBaseInterruptBehavior.interupt_next_only = True
+    def find_max_x_load(self):
+        max = 0
+        for dataset in self.datasets:
+            if(dataset.x_load > max):
+                max = dataset.x_load
+        return max
+
+    def update_dataset(self, obj):
+        self.second_counter += 1
+        time_delta = datetime.datetime.now() - self.start_time
+        total_time_passed = time_delta.seconds + (time_delta.microseconds * .000001)
+        self.test_time = time_delta.seconds
+        if self.second_counter >= SECOND_CAP/2:
+            self.double_counter += 1
+            self.second_counter = 0
+            self.graph1 = self.ids['graph_test1']
+            self.graph2 = self.ids['graph_test2']
+            self.graph1.remove_plot(self.plot1)
+            self.graph2.remove_plot(self.plot2)
+            self.graph1._clear_buffer()
+            self.graph2._clear_buffer()
+            self.plot1 = MeshLinePlot(color=[1, 1, 1, 1])
+            self.plot2 = MeshLinePlot(color=[1, 1, 1, 1])
+            last_index = len(self.datasets) - 1
+            
+            self.x_max = math.ceil(self.datasets[last_index].timestamp / 5) * 5
+            self.y_min1 = max(self.y_min1, math.ceil(self.datasets[last_index].strain1)*0.8)
+            self.y_min2 = max(self.y_min2, math.ceil(self.datasets[last_index].strain2)*0.8)
+            self.y_max1 = max(self.y_max1, math.ceil(self.datasets[last_index].strain1)*1.2)
+            self.y_max2 = max(self.y_max2, math.ceil(self.datasets[last_index].strain2)*1.2)
+            
+
+            self.x_major = int((self.x_max-self.x_min)/5)
+            self.y_major1 = int((self.y_max1-self.y_min1)/5)
+            self.y_major2 = int((self.y_max2-self.y_min2)/5)
+
+            self.plot1.points = [(self.datasets[i].timestamp, self.datasets[i].strain1) for i in range(0, len(self.datasets), 5)]
+            self.plot2.points = [(self.datasets[i].timestamp, self.datasets[i].strain2) for i in range(0, len(self.datasets), 5)]
+            
+            self.graph1.add_plot(self.plot1)
+            self.graph2.add_plot(self.plot2)
+
+        sensor_values = self.test_sensor.get_sensor_data(adc_out=1) # FIXME THIS GETS RAW VOLTAGES
+        self.x_load = sensor_values["X Load"]
+        self.y_load = sensor_values["Y Load"]
+        self.pot_angle = sensor_values["Pot Angle"]
+        self.imu_angle = sensor_values["IMU Angle"]
+        self.strain1 = sensor_values["Strain 1"]
+        self.strain2 = sensor_values["Strain 2"]
+
+        new_dataset = Dataset(total_time_passed, self.x_load, self.y_load, self.pot_angle, self.imu_angle, self.data_rate, self.strain1, self.strain2)
+        self.datasets.append(new_dataset)
+
+    def on_pre_leave(self):
+        self.event.cancel()
+        ts = TestSingleton()
+        ts.clear_all()
+        ts.set_height(str(config.get('height', "")))
+        ts.set_plot(str(config.get('plot_num', "")))
+        config.set('break_height', "N/A")
+
+        ts.set_operator(str(config.get('operator', "")))
+        ts.set_timestamp(self.start_timestamp)
+        ts.set_datasets(self.datasets)
+        self.datasets = []
+        self.graph1.remove_plot(self.plot1)
+        self.graph1._clear_buffer()
+        self.graph2.remove_plot(self.plot2)
+        self.graph2._clear_buffer()
---
+++ Granusoft/src_new/Arm/Screens/DataCollection/ARM_TestSettingsConfirmation.py
@@ -0,0 +1,27 @@
+"""
+Test settings confirmation menu. This is to make sure the user sets the correct
+data for the test.
+"""
+
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+from util.BaseScreen import BaseScreen
+import Arm.Settings.configurator as config
+from util.elements import *
+from util.getKVPath import getKVPath
+import os
+
+Builder.load_file(getKVPath(os.getcwd(), __file__))
+
+class ARM_TestSettingsConfirmation(BaseScreen):
+    height_num = StringProperty("N/A")
+    plot = StringProperty("N/A")
+    operator = StringProperty("N/A")
+
+    def on_pre_enter(self):
+        """Before the Screen loads, read the configuration file to get the current
+        list of notes. Show the default buttons."""
+        self.height_num = str(config.get('height',0))
+        self.plot = str(config.get('plot_num',0))
+        self.operator = str(config.get('operator','N/A'))
