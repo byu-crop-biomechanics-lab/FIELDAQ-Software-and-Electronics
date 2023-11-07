3ad2c09b4558af6959c0339ba2194d1c864d1969
Caleb Price <calebsp12@gmail.com>
2023-11-07 09:20:14 -0700

    Updated Rod

---
+++ Granusoft/src_new/Rodney/Screens/DataCollection/ROD_TestFoldersScreen.py
@@ -0,0 +1,109 @@
+"""
+Test in Progress
+"""
+
+
+from genericpath import isdir
+from kivy.lang import Builder
+
+from kivy.properties import ListProperty
+from kivy.properties import ObjectProperty
+from kivy.clock import Clock
+import Arm.Settings.configurator as config
+
+from Arm.Data.TestSingleton import TestSingleton
+from shutil import copyfile
+import datetime
+
+from kivy.uix.popup import Popup
+
+from util.BaseScreen import BaseScreen
+from util.SingleSelectableList import SingleSelectableList, SingleSelectableListBehavior, SingleSelectableRecycleBoxLayout
+from util.elements import *
+import os
+from os import listdir
+from os.path import isfile, join
+
+from kivy.garden.graph import Graph, MeshLinePlot
+from util.getKVPath import getKVPath
+
+Builder.load_file(getKVPath(os.getcwd(), __file__))
+
+FOLDERNAME = "Tests/"+"config.get('selected_folder',0)"
+
+class ROD_Folder(SingleSelectableListBehavior, Label):
+    pass
+
+
+class ROD_NavButton(Button):
+    pass
+
+
+class ROD_FolderList(SingleSelectableList):
+    def update(self, k, val):
+        self.data = [{'text': str(x)} for x in self.list_data]
+
+
+class ROD_TestFoldersScreen(BaseScreen):
+    USB_TEST_FOLDERS_PATH = '/mnt/usbStick'
+
+    def __init__(self, **kwargs):
+        super(BaseScreen, self).__init__(**kwargs)
+        self.back_button = GranuSideButton(text='Back')
+        self.back_button.bind(on_release=self.go_back)
+        self.delete_button = GranuSideButton(text='Delete\nFolder')
+        self.delete_button.bind(on_release=self.delete_folder)
+        self.view_tests_button = GranuSideButton(text='View\nTests')
+        self.view_tests_button.bind(on_release=self.view_tests)
+        self.rename_folder_button = GranuSideButton(text='Rename\nFolder')
+        self.rename_folder_button.bind(on_release=self.rename_folder)
+
+        def gui_init(dt):
+            self.test_screen = self.manager.get_screen('tests_screen')
+            self.parent_screen = self
+        Clock.schedule_once(gui_init)
+
+    def on_pre_enter(self):
+        self.test_folder_names = [f for f in listdir("Tests") if (
+            isdir(join("Tests", f)) and f != ".gitignore")]
+
+        self.default_folders_buttons()
+
+        self.ids['folders_list'].list_data = self.test_folder_names
+        self.folder_list = self.ids['folders_list']
+
+    def go_back(self, obj):
+        super(ROD_TestFoldersScreen, self).back()
+
+    def delete_folder(self, obj):
+        selected = self.folder_list.get_selected()
+        config.set('unwanted_folder', str(selected[0]))
+        self.parent_screen.move_to('rod_delete_folder_confirmation')
+
+    def view_tests(self, obj):
+        selected = self.folder_list.get_selected()
+        config.set('selected_folder', str(selected[0]))
+        self.parent_screen.move_to('tests_screen')
+
+    def rename_folder(self,obj):
+        selected = self.folder_list.get_selected()
+        config.set('selected_folder', str(selected[0]))
+        self.parent_screen.move_to('rename_folder_screen')
+
+    def default_folders_buttons(self):
+        buttons = self.ids['folders_buttons']
+        buttons.clear_widgets()
+        buttons.add_widget(self.back_button)
+        buttons.add_widget(Widget())
+
+    def folder_buttons(self):
+        buttons = self.ids['folders_buttons']
+        buttons.clear_widgets()
+        buttons.add_widget(self.back_button)
+        buttons.add_widget(self.view_tests_button)
+        buttons.add_widget(self.rename_folder_button)
+        buttons.add_widget(self.delete_button)
+
+    def on_leave(self):
+        if os.path.ismount(self.USB_TEST_FOLDERS_PATH):
+            os.system("sudo umount /mnt/usbStick")
---
+++ Granusoft/src_new/Rodney/Screens/Settings/ROD_TestDeleteConfirmation.py
@@ -0,0 +1,30 @@
+"""
+This screen makes sure that the user will not accidentally delete tests stored on the device. It is quite simple
+with only two buttons, delete and cancel. The position and type of button is intentionally different than the previous
+set of buttons so it is very obvious there was a change in screen. 
+"""
+
+import os
+
+from kivy.lang import Builder
+
+from util.BaseScreen import BaseScreen
+from os import listdir
+from os.path import isfile, join
+
+from util.getKVPath import getKVPath
+
+Builder.load_file(getKVPath(os.getcwd(), __file__))
+
+class ROD_TestDeleteConfirmation(BaseScreen):
+    def on_pre_enter(self):
+        self.test_filenames = [f for f in listdir("TestArchive") if (isfile(join("TestArchive", f)) and f != ".gitignore")]
+
+    def remove_all(self):
+        for name in self.test_filenames:
+            if name != '.gitignore':
+                os.remove('TestArchive/' + name)
+        super(ROD_TestDeleteConfirmation, self).back()
+
+    def cancel(self):
+        super(ROD_TestDeleteConfirmation, self).back()
