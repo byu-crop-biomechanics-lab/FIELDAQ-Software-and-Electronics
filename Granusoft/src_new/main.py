29f214365426fad2fa75c8d8744494d7e4e9bbab
Nathan Ludlow <Nateludlow@gmail.com>
2023-11-10 09:51:48 -0700

    Add darling (#7)
    Added Darling Device to Mode select. Fixed all Modes so that they are running properly. 
    
    ---------
    
    Co-authored-by: Caleb Price <calebsp12@gmail.com>

--- Granusoft/src_new/main.py
+++ Granusoft/src_new/main.py
@@ -1,8 +1,6 @@
 from kivy.config import Config as KivyConfig
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, FadeTransition, NoTransition
-from Arm.Screens.ExitScreen import ExitScreen
-
 
 # Kivy Configuration
 KivyConfig.set('kivy', 'desktop', 0) # Disable OS-specific features for testing
@@ -26,8 +24,14 @@ class MainApp(App):
 if __name__ == "__main__":
     import sys
 
+
     root_directory = sys.base_prefix
     print("Root Python directory:", root_directory)
 
     # Run the App
-    MainApp().run()
+    try:
+        MainApp().run()
+    except KeyboardInterrupt:
+        print("Closing Application...")
+        sys.exit()
+
