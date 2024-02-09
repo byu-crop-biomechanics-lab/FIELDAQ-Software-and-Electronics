e47ea08509a75eaef860cf8296eba28952abba91
Caleb Price <106776391+pr1ce1227@users.noreply.github.com>
2024-02-09 09:28:10 -0700

    Folder system (#11)
    * Fixed ArchiveFolder
    
    * note screen fix
    
    * Fixed many of the file path issues
    
    * Basic file structure working
    
    * Fixed archiving problem
    
    * Files seem to be working now
    
    * Added necessary folders
    
    * Continue
    
    * Update TODO
    
    * Rename folder funciton is working
    
    * Fixed file system, all buttons work, Also fixed config file so that it doesn't delete at startup
    
    * Fixed ranaming so that it updated config file correctly
    
    * Rodney working, needs to have sensors added
    
    * Fixed configurator path
    
    * continue
    
    * Fixed x_load path
    
    * added init, doesn't read and write yet
    
    * continue
    
    * commit 1
    
    * commit 2
    
    * commit 3
    
    * 4
    
    * commit 5
    
    * made rodney class config a singleton
    
    * 8
    
    * 9
    
    * Fixed barcode
    
    * Fixed height
    
    * a
    
    * b
    
    * 31
    
    * 32
    
    * 33
    
    * 55
    
    * 56
    
    * working version with config file that saves and is not overwritten
    
    * 57
    
    * working version with file saving
    
    * not completed changes
    
    * removed duplication
    
    * removed duplication
    
    * added mux and adcs
    
    * added imports
    
    * fixing
    
    * code stuff
    
    * read code is added for adcs
    
    * updating file writing
    
    * updated stuff
    
    * almost working code
    
    * working code almost, but it reads from the ADCS
    
    * working code. Requires new code for calibration, and live display
    
    * added whiskers file
    
    * Added 2nd whisker
    
    * removed strain8 from sensors screen
    
    * working sensor system
    
    * cleaning
    
    * added placeholder constants for rodney calibration
    
    * updated todo list
    
    * updated todo list
    
    * stuff
    
    * Fixed adc get data
    
    * updated strain stuff
    
    * Fixed calibrate screen
    
    ---------
    
    Co-authored-by: Jeremy <jeremyr6119@gmail.com>

--- Granusoft/src_new/Devices/Rodney/Sensors/SensorFake.py
+++ Granusoft/src_new/Devices/Rodney/Sensors/SensorFake.py
@@ -6,7 +6,7 @@ class Sensor:
 
     def __init__(self):
         self.REAL_DATA = False
-        self.keys = ["Temperature","Humidity","Location","Time","strain8","whiskers","IMU Angle", "Load Cell Height"]
+        self.keys = ["Temperature","Humidity","Location","Time","strain8","WhiskerFront", "WhiskerBack","IMU Angle", "Load Cell Height"]
         self.time = datetime.datetime.now().strftime("%I:%M:%S %p")
         self.sensor_data = {}
         self.cpu_time = 0
@@ -36,8 +36,8 @@ class Sensor:
         self.strain8_fake[1] = 900 + math.fabs(90 - math.fmod(2*(self.elapsed_time - 90/2), 2*90))
         self.strain8_fake[2] = 900 + math.fabs(90 - math.fmod(2*(self.elapsed_time - 90/2), 2*90))
         self.strain8_fake[3] = 900 + math.fabs(90 - math.fmod(2*(self.elapsed_time - 90/2), 2*90))
-        self.whiskers_fake[0] = 9000 + math.fabs(90 - math.fmod(2*(self.elapsed_time - 90/2), 2*90))
-        self.whiskers_fake[1] = 9000 + math.fabs(90 - math.fmod(2*(self.elapsed_time - 90/2), 2*90))
+        self.whiskers_front = 9000 + math.fabs(90 - math.fmod(2*(self.elapsed_time - 90/2), 2*90))
+        self.whiskers_back = 9000 + math.fabs(90 - math.fmod(2*(self.elapsed_time - 90/2), 2*90))
         self.imu_fake = 90 + math.fabs(90 - math.fmod(2*(self.elapsed_time - 90/2.1), 2*90))
         self.elapsed_time += 0.1
         self.sensor_data["Time"] = self.time
